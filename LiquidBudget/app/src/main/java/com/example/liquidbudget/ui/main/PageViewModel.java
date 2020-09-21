00976465dacdbacdbdbe204a12966e825ab7cbbd
Caroline Gabriel <cmg9522@utulsa.edu>
2020-09-20 23:55:35 -0500

    Added text to homepage

--- LiquidBudget/app/src/main/java/com/example/liquidbudget/ui/main/PageViewModel.java
+++ LiquidBudget/app/src/main/java/com/example/liquidbudget/ui/main/PageViewModel.java
@@ -12,7 +12,9 @@ public class PageViewModel extends ViewModel {
     private LiveData<String> mText = Transformations.map(mIndex, new Function<Integer, String>() {
         @Override
         public String apply(Integer input) {
-            return "Hello world from section: " + input;
+            return "" ;
+            //return "Hello world from section: " + input;
+            //Figuring this out still -Caroline
         }
     });
 
