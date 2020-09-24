a58a280a54f03b540046bad4e20360470adbf87c
weston <westonphillips123@gmail.com>
2020-09-24 14:25:56 -0500

    Fixed Spending/Saving Page
    Fixed the taskbar issue, and added more code to make the Pie Chart look better

--- LiquidBudget/app/src/main/java/com/example/liquidbudget/SpendingSavingPage.java
+++ LiquidBudget/app/src/main/java/com/example/liquidbudget/SpendingSavingPage.java
@@ -1,25 +1,15 @@
 package com.example.liquidbudget;
 
 import android.os.Bundle;
-
 import com.anychart.AnyChart;
 import com.anychart.AnyChartView;
 import com.anychart.chart.common.dataentry.DataEntry;
 import com.anychart.chart.common.dataentry.ValueDataEntry;
 import com.anychart.charts.Pie;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.tabs.TabLayout;
+import com.anychart.enums.Align;
+import com.anychart.enums.LegendLayout;
 
-import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-
-import com.example.liquidbudget.ui.main.SectionsPagerAdapter;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,8 +17,9 @@ public class SpendingSavingPage extends AppCompatActivity {
 
     AnyChartView anyChartView;
 
-    String[] categories = {"Food/Drink", "Utilities", "Miscellaneous", "Groceries", "Gas", "Saved"};
-    int[] spending = {75, 125, 45, 80, 50, 230};
+    // Creating Two Arrays with the data for the Pie Chart
+    String[] categories = {"Food/Drink", "Utilities", "Miscellaneous", "Saved"};
+    int[] spending = {75, 125, 80, 230};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,19 +28,33 @@ public class SpendingSavingPage extends AppCompatActivity {
 
         anyChartView = findViewById(R.id.any_chart_view);
 
+        // Setting Up Pie Chart
         setupPieChart();
     }
 
     public void setupPieChart() {
+        // Creating Pie Chart
         Pie pie = AnyChart.pie();
         List<DataEntry> dataEntries = new ArrayList<>();
 
+        // Inputted the data from the two arrays into a list
         for(int i=0; i<categories.length; i++) {
             dataEntries.add(new ValueDataEntry(categories[i], spending[i]));
         }
+
+        // Inserting that data into the Pie Chart
         pie.data(dataEntries);
+
+        // Formatting the Chart
+        pie.title("Spending Categories for July 2020");
+        pie.labels().position("outside");
+
+        pie.legend().position("center-bottom");
+        pie.legend().itemsLayout(LegendLayout.HORIZONTAL);
+        pie.legend().align(Align.CENTER);
+
+        pie.credits().enabled(false);
         anyChartView.setChart(pie);
     }
-
 
 }
