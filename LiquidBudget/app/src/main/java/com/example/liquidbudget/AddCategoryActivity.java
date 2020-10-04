aa6c2acca972bda30005f8e700979e17a6ef31a3
Tim Sun <tianyi.sun4@gmail.com>
2020-10-04 16:12:56 -0500

    Update AddCategoryActivity.java
    makes the activity CLOSE instead of starting a new activity on the stack

--- LiquidBudget/app/src/main/java/com/example/liquidbudget/AddCategoryActivity.java
+++ LiquidBudget/app/src/main/java/com/example/liquidbudget/AddCategoryActivity.java
@@ -1,6 +1,5 @@
 package com.example.liquidbudget;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,8 +15,8 @@ public class AddCategoryActivity extends AppCompatActivity{
         createCategory.setOnClickListener(new View.OnClickListener(){
         @Override
         public void onClick(View view){
-            Intent startIntent = new Intent(getApplicationContext(), CategoryActivity.class);
-            startActivity(startIntent);
+            finish();
+            return;
         }
     });
 
