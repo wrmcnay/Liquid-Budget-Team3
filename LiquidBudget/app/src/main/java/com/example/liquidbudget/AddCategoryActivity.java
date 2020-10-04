00b585c942266d007a4d0d4b966e8331beaa4a42
Tim Sun <tianyi.sun4@gmail.com>
2020-10-04 16:00:55 -0500

    added features to categories

--- LiquidBudget/app/src/main/java/com/example/liquidbudget/AddCategoryActivity.java
+++ LiquidBudget/app/src/main/java/com/example/liquidbudget/AddCategoryActivity.java
@@ -1,10 +1,25 @@
 package com.example.liquidbudget;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AddCategoryActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.category_add);
+
+    Button createCategory = (Button)findViewById(R.id.createCategory);
+        createCategory.setOnClickListener(new View.OnClickListener(){
+        @Override
+        public void onClick(View view){
+            Intent startIntent = new Intent(getApplicationContext(), CategoryActivity.class);
+            startActivity(startIntent);
+        }
+    });
+
     }
 }
