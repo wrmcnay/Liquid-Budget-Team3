109e72973eaa2d20247e04272a7b1ead7c7fd712
Riley McNay <wrmcnay@att.net>
2020-10-11 14:54:32 -0500

    adding settings buttons

--- LiquidBudget/app/src/main/java/com/example/liquidbudget/settings/AccountActivity.java
+++ LiquidBudget/app/src/main/java/com/example/liquidbudget/settings/AccountActivity.java
@@ -3,19 +3,14 @@ package com.example.liquidbudget.settings;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 
 import com.example.liquidbudget.R;
 
-public class SettingsActivity extends AppCompatActivity {
+public class AccountActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
-    }
-
-    public void goToAccount(View view) {
-
+        setContentView(R.layout.activity_account);
     }
 }
