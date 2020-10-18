bca0df3f4e688c16be4a88fade03287914609abd
sisaak <shaneisaak12@gmail.com>
2020-10-18 16:42:31 -0500

    implemented income/expense add UI

---
+++ LiquidBudget/app/src/main/java/com/example/liquidbudget/AddIncomeExpenseActivity.java
@@ -0,0 +1,14 @@
+package com.example.liquidbudget;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddIncomeExpenseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.expense_income_add);
+    }
+}
