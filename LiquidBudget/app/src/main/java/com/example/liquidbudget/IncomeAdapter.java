9b4b662c94288b198bdb21e4b573d27d91eaee70
weston <westonphillips123@gmail.com>
2020-10-20 10:04:25 -0500

    Created Entity, DAO, Database, Repository, and ViewModel files

---
+++ LiquidBudget/app/src/main/java/com/example/liquidbudget/IncomeAdapter.java
@@ -0,0 +1,26 @@
+package com.example.liquidbudget;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.liquidbudget.data.model.Income;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IncomeAdapter  {//extends RecyclerView.Adapter<IncomeAdapter.IncomeHolder> {
+
+    //private List<Income> incomes = new ArrayList<>();
+
+   // @NonNull
+   // @Override
+  //  public IncomeHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+       // View itemView = LayoutInflater.from(parent.getContext())
+                //.inflate(R.layout.u)
+   // }
+}
