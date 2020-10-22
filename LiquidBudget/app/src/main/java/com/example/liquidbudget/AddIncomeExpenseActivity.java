1b0d1d21581552113f0b0b32b14a1bf49f8c2b52
sisaak <shaneisaak12@gmail.com>
2020-10-22 15:35:06 -0500

    Update AddIncomeExpenseActivity.java

--- LiquidBudget/app/src/main/java/com/example/liquidbudget/AddIncomeExpenseActivity.java
+++ LiquidBudget/app/src/main/java/com/example/liquidbudget/AddIncomeExpenseActivity.java
@@ -7,11 +7,19 @@ import android.view.View;
 import android.widget.Button;
 
 public class AddIncomeExpenseActivity extends AppCompatActivity {
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.expense_income_add);
+
+        Button createIncExp = (Button)findViewById(R.id.buttonSubmit);
+        createIncExp.setOnClickListener(new View.OnClickListener(){
+        @Override
+        public void onClick(View view){
+            setContentView(R.layout.activity_main);
+            finish();
+            return;
+        }
+    });
+
     }
-    
 }
