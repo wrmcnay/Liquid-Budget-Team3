d8afd015d003cebd133c2df3ceb7eca27c773140
sisaak <shaneisaak12@gmail.com>
2020-11-04 16:37:27 -0600

    Expense entity

---
+++ LiquidBudget/app/src/main/java/com/example/liquidbudget/data/database/ExpenseDatabase.java
@@ -0,0 +1,4 @@
+package com.example.liquidbudget.data.Database;
+
+public class ExpenseDatabase {
+}
---
+++ LiquidBudget/app/src/main/java/com/example/liquidbudget/data/entities/Expense.java
@@ -0,0 +1,164 @@
+package com.example.liquidbudget.data.entities;
+
+import androidx.annotation.NonNull;
+import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
+import androidx.room.ColumnInfo;
+
+@Entity(tableName = "incomes")
+public class Expense {
+    @NonNull
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "expenseID")
+    private int expenseID;
+
+    @NonNull
+    @ColumnInfo(name = "categoryID")
+    private int categoryID;
+
+    @NonNull
+    @ColumnInfo(name = "expenseName")
+    private String expenseName;
+
+    @ColumnInfo(name = "categoryName")
+    private String categoryName;
+
+    @ColumnInfo(name = "amount")
+    private double amount;
+
+    @ColumnInfo(name = "recurringBool")
+    private boolean recurringBool;
+
+    @ColumnInfo(name = "numberOf")
+    private int numberOf;
+
+    @ColumnInfo(name = "period")
+    private int period;
+
+    @ColumnInfo(name = "stable")
+    private boolean stable;
+
+    @ColumnInfo(name = "date")
+    private String date;
+
+    @NonNull
+    @ColumnInfo(name = "userID")
+    private int userID;
+
+    public Expense(String expenseName, String categoryName, double amount) {
+        this.expenseName = expenseName;
+        this.categoryName = categoryName;
+        this.amount = amount;
+    }
+
+    @Ignore
+    public Expense(int expenseID, int categoryID, String expenseName, String categoryName, double amount, boolean recurringBool, int numberOf, int period, boolean stable, String date, int userID) {
+        this.expenseID = expenseID;
+        this.categoryID = categoryID;
+        this.expenseName = expenseName;
+        this.categoryName = categoryName;
+        this.amount = amount;
+        this.recurringBool = recurringBool;
+        this.numberOf = numberOf;
+        this.period = period;
+        this.stable = stable;
+        this.date = date;
+        this.userID = userID;
+    }
+
+    public int getExpenseID() {
+        return expenseID;
+    }
+
+    public void setExpenseID(int expenseID) {
+        this.expenseID = expenseID;
+    }
+
+    public int getCategoryID() {
+        return categoryID;
+    }
+
+    public void setCategoryID(int categoryID) {
+        this.categoryID = categoryID;
+    }
+
+    public String getExpenseName() {
+        return expenseName;
+    }
+
+    public void setExpenseName(String expenseName) {
+        this.expenseName = expenseName;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public boolean isRecurringBool() {
+        return recurringBool;
+    }
+
+    public void setRecurringBool(boolean recurringBool) {
+        this.recurringBool = recurringBool;
+    }
+
+    public int getNumberOf() {
+        return numberOf;
+    }
+
+    public void setNumberOf(int numberOf) {
+        this.numberOf = numberOf;
+    }
+
+    public int getPeriod() {
+        return period;
+    }
+
+    public void setPeriod(int period) {
+        this.period = period;
+    }
+
+    public boolean isStable() {
+        return stable;
+    }
+
+    public void setStable(boolean stable) {
+        this.stable = stable;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    @Override
+    public String toString() {
+        return new StringBuilder(expenseName).append("\n").append(categoryName).append("\n").append(amount).toString();
+    }
+}
+
+
