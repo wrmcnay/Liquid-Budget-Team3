7d5f71cc6615d9b734fb195b2b4c9f6f70366e9f
Tim Sun <tianyi.sun4@gmail.com>
2020-11-05 09:55:19 -0600

    should be able to run with these changes

--- LiquidBudget/app/src/main/java/com/example/liquidbudget/MainActivity.java
+++ LiquidBudget/app/src/main/java/com/example/liquidbudget/MainActivity.java
@@ -1,19 +1,10 @@
 package com.example.liquidbudget;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
 
-import com.example.liquidbudget.settings.SettingsActivity;
 import com.example.liquidbudget.ui.main.AppBaseActivity;
-import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppBaseActivity {
 
@@ -23,9 +14,5 @@ public class MainActivity extends AppBaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.homepage_test);
-
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
     }
 }
