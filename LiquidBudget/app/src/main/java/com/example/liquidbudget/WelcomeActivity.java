b3bdfc10f97516e3abf666220b628d6939564ccb
Riley McNay <wrmcnay@att.net>
2020-11-06 12:50:42 -0600

    Populating email on successful sign in

--- LiquidBudget/app/src/main/java/com/example/liquidbudget/WelcomeActivity.java
+++ LiquidBudget/app/src/main/java/com/example/liquidbudget/WelcomeActivity.java
@@ -1,13 +1,13 @@
 package com.example.liquidbudget;
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -60,7 +60,7 @@ public class WelcomeActivity extends AppCompatActivity {
             Intent intent = new Intent(this, MainActivity.class);
             startActivity(intent);
         }
-    }
+}
 
     public void launchSignIn(View view){
         Intent intent = mGoogleSignInClient.getSignInIntent();
--- LiquidBudget/app/src/main/java/com/example/liquidbudget/settings/SettingsActivity.java
+++ LiquidBudget/app/src/main/java/com/example/liquidbudget/settings/SettingsActivity.java
@@ -4,9 +4,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.liquidbudget.R;
 import com.example.liquidbudget.ui.main.AppBaseActivity;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 
 public class SettingsActivity extends AppBaseActivity {
 
@@ -59,5 +62,12 @@ public class SettingsActivity extends AppBaseActivity {
                 startActivity(startIntent);
             }
         });
+
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        if (account != null) {
+            String userEmail = account.getEmail();
+            TextView email = findViewById(R.id.emailEditText);
+            email.setText("Email: " + userEmail);
+        }
     }
 }
