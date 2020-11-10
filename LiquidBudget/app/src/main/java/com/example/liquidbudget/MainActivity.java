e162adbb08bf0918824b4427f25dd445f954d900
carolinegabriel <32170517+carolinegabriel@users.noreply.github.com>
2020-11-09 18:22:55 -0600

    Homepage greets user with their Google display name

--- LiquidBudget/app/src/main/java/com/example/liquidbudget/MainActivity.java
+++ LiquidBudget/app/src/main/java/com/example/liquidbudget/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.liquidbudget;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.widget.Toolbar;
 
 import com.example.liquidbudget.ui.main.AppBaseActivity;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 
 public class MainActivity extends AppBaseActivity {
 
@@ -14,6 +18,13 @@ public class MainActivity extends AppBaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        if (account != null) {
+            String userName = account.getDisplayName();
+            TextView homepagehello = findViewById(R.id.homepagehello);
+            homepagehello.setText(getString(R.string.homepage_hello, userName));
+        }
     }
 
 }
