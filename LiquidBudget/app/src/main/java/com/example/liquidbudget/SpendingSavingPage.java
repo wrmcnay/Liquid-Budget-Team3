c6557ca65728b015fdca01752bc54db71671a904
weston <westonphillips123@gmail.com>
2020-11-09 20:21:34 -0600

    Adding GraphView to build.gradle file

--- LiquidBudget/app/src/main/java/com/example/liquidbudget/SpendingSavingPage.java
+++ LiquidBudget/app/src/main/java/com/example/liquidbudget/SpendingSavingPage.java
@@ -1,61 +1,182 @@
 package com.example.liquidbudget;
 
-import android.os.Bundle;
-
-import com.anychart.AnyChart;
-import com.anychart.AnyChartView;
-import com.anychart.chart.common.dataentry.DataEntry;
 import com.anychart.chart.common.dataentry.ValueDataEntry;
-import com.anychart.charts.Pie;
-import com.anychart.enums.Align;
-import com.anychart.enums.LegendLayout;
 import com.example.liquidbudget.ui.main.AppBaseActivity;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.RelativeSizeSpan;
+import android.text.style.StyleSpan;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.RelativeLayout;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.core.content.ContextCompat;
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.DefaultAxisValueFormatter;
+import com.github.mikephil.charting.formatter.PercentFormatter;
+import com.github.mikephil.charting.formatter.ValueFormatter;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.github.mikephil.charting.interfaces.datasets.IDataSet;
+import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+import com.github.mikephil.charting.utils.ColorTemplate;
+import com.github.mikephil.charting.utils.MPPointF;
+
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
-public class SpendingSavingPage extends AppBaseActivity {
-
-    AnyChartView anyChartView;
-
-    // Creating Two Arrays with the data for the Pie Chart
-    String[] categories = {"Food/Drink", "Utilities", "Miscellaneous", "Saved"};
-    int[] spending = {75, 125, 80, 230};
+public class SpendingSavingPage extends AppBaseActivity implements OnChartValueSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spending_saving_page);
 
-        anyChartView = findViewById(R.id.any_chart_view);
+        setTitle("Spending Vs Savings");
 
-        // Setting Up Pie Chart
-        setupPieChart();
+        PieChart chart = findViewById(R.id.pie_chart);
+
+        // setting the data values
+        ArrayList<PieEntry> entries = new ArrayList<>();
+
+        entries.add(new PieEntry((float) 389.82, "Amount Saved"));
+        entries.add(new PieEntry((float) 168.54, "Groceries"));
+        entries.add(new PieEntry((float) 112.92, "Utilities"));
+        entries.add(new PieEntry((float) 204.35, "Food & Drink"));
+        entries.add(new PieEntry((float) 156.23, "Other"));
+
+        PieDataSet dataSet = new PieDataSet(entries, "");
+
+        dataSet.setDrawIcons(true);
+
+        dataSet.setSliceSpace(3f);
+        dataSet.setIconsOffset(new MPPointF(0, 40));
+        dataSet.setSelectionShift(5f);
+
+        // add a lot of colors
+
+        ArrayList<Integer> colors = new ArrayList<>();
+
+        for (int c : ColorTemplate.VORDIPLOM_COLORS)
+            colors.add(c);
+
+        for (int c : ColorTemplate.LIBERTY_COLORS)
+            colors.add(c);
+
+        for (int c : ColorTemplate.COLORFUL_COLORS)
+            colors.add(c);
+
+        for (int c : ColorTemplate.LIBERTY_COLORS)
+            colors.add(c);
+
+        for (int c : ColorTemplate.PASTEL_COLORS)
+            colors.add(c);
+
+        colors.add(ColorTemplate.getHoloBlue());
+        dataSet.setColors(colors);
+
+        PieData pieData = new PieData(dataSet);
+        pieData.setValueFormatter(new DefaultAxisValueFormatter(2));
+        pieData.setValueTextSize(11f);
+        pieData.setDrawValues(true);
+        pieData.setValueTextColor(Color.BLACK);
+
+        chart.setData(pieData);
+        chart.getDescription().setEnabled(true);
+        chart.setCenterText(generateCenterSpannableText());
+
+        dataSet.setDrawIcons(true);
+
+        dataSet.setSliceSpace(5f);
+        dataSet.setIconsOffset(new MPPointF(0, 40));
+        dataSet.setSelectionShift(5f);
+
+        chart.setUsePercentValues(false);
+        chart.getDescription().setEnabled(false);
+        chart.setExtraOffsets(5, 10, 5, 5);
+
+        chart.setDragDecelerationFrictionCoef(0.95f);
+
+        chart.setDrawHoleEnabled(true);
+        chart.setHoleColor(Color.WHITE);
+
+        chart.setTransparentCircleColor(Color.WHITE);
+        chart.setTransparentCircleAlpha(110);
+
+        chart.setHoleRadius(38f);
+        chart.setTransparentCircleRadius(44f);
+
+        chart.setDrawCenterText(true);
+
+        chart.setRotationAngle(0);
+        // enable rotation of the chart by touch
+        chart.setRotationEnabled(true);
+        chart.setHighlightPerTapEnabled(true);
+
+        chart.setOnChartValueSelectedListener((OnChartValueSelectedListener) this);
+
+        chart.animateY(1800, Easing.EaseInOutQuad);
+
+        Legend l = chart.getLegend();
+        l.setVerticalAlignment(Legend.LegendVerticalAlignment.TOP);
+        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.LEFT);
+        l.setOrientation(Legend.LegendOrientation.VERTICAL);
+        l.setDrawInside(false);
+        l.setFormSize(20);
+        l.setFormToTextSpace(5);
+        l.setTextSize(12);
+        l.setXEntrySpace(10f);
+        l.setYEntrySpace(4f);
+        l.setYOffset(0f);
+        l.setWordWrapEnabled(true);
+
+        // entry label styling
+        chart.setEntryLabelColor(Color.BLACK);
+        chart.setEntryLabelTextSize(16f);
+        chart.setDrawEntryLabels(true);
+
+        chart.invalidate();
+
+        generateCenterSpannableText();
     }
 
-    public void setupPieChart() {
-        // Creating Pie Chart
-        Pie pie = AnyChart.pie();
-        List<DataEntry> dataEntries = new ArrayList<>();
-
-        // Inputted the data from the two arrays into a list
-        for(int i=0; i<categories.length; i++) {
-            dataEntries.add(new ValueDataEntry(categories[i], spending[i]));
-        }
-
-        // Inserting that data into the Pie Chart
-        pie.data(dataEntries);
-
-        // Formatting the Chart
-        pie.title("Spending Categories for July 2020");
-        pie.labels().position("outside");
-
-        pie.legend().position("center-bottom");
-        pie.legend().itemsLayout(LegendLayout.HORIZONTAL);
-        pie.legend().align(Align.CENTER);
-
-        pie.credits().enabled(false);
-        anyChartView.setChart(pie);
+    private SpannableString generateCenterSpannableText() {
+        SpannableString s = new SpannableString("TOTAL SPENT\n$642.04");
+        s.setSpan(new RelativeSizeSpan(2f), 11, s.length(), 0);
+        s.setSpan(new StyleSpan(Typeface.BOLD), 11, s.length(), 0);
+        return s;
     }
+
+    public void onValueSelected(Entry e, Highlight h) {
+
+        if (e == null)
+            return;
+        Log.i("VAL SELECTED",
+                "Value: " + e.getY() + ", index: " + h.getX()
+                        + ", DataSet index: " + h.getDataSetIndex());
+    }
+
+    @Override
+    public void onNothingSelected() {
+
+    }
+
 
 }
