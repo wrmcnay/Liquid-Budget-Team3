8b3fd846e55d66699e1f0b1be350ac5439f68645
weston <westonphillips123@gmail.com>
2020-11-09 20:32:25 -0600

    Revert "Revert "Merge branch 'master' into Weston""
    This reverts commit 920046f72d68c455f7fe1a66510755bc1778c16b.

--- LiquidBudget/app/src/main/java/com/example/liquidbudget/MainActivity.java
+++ LiquidBudget/app/src/main/java/com/example/liquidbudget/MainActivity.java
@@ -1,84 +1,30 @@
 package com.example.liquidbudget;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
+import android.widget.TextView;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
 
-import com.example.liquidbudget.settings.SettingsActivity;
-import com.google.android.material.navigation.NavigationView;
+import com.example.liquidbudget.ui.main.AppBaseActivity;
 
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 
-    DrawerLayout drawer;
-    NavigationView navigationView;
-    ActionBarDrawerToggle toggle;
+public class MainActivity extends AppBaseActivity {
+
     Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_homepage);
 
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-        navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-
-    }
-
-    public void onBackPressed() {
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        if (account != null) {
+            String userName = account.getDisplayName();
+            TextView homepagehello = findViewById(R.id.homepagehello);
+            homepagehello.setText(getString(R.string.homepage_hello, userName));
         }
     }
 
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        int id = item.getItemId();
-        switch (id) {
-            case R.id.nav_home:
-//                Intent homeIntent = new Intent(MainActivity.this, MainActivity.class);
-//                startActivity(homeIntent);
-                break;
-            case R.id.nav_budget:
-                Intent budgetIntent = new Intent(MainActivity.this, SpendingSavingPage.class);
-                startActivity(budgetIntent);
-                break;
-            case R.id.nav_category:
-                Intent catIntent = new Intent(MainActivity.this, CategoryActivity.class);
-                startActivity(catIntent);
-                break;
-            case R.id.nav_settings:
-                Intent settingIntent = new Intent(MainActivity.this, SettingsActivity.class);
-                startActivity(settingIntent);
-                break;
-            case R.id.nav_users:
-                Intent usersIntent = new Intent(MainActivity.this, UserDisplayActivity.class);
-                startActivity(usersIntent);
-                break;
-            case R.id.nav_incExp:
-                Intent addIncExpIntent = new Intent(MainActivity.this, AddIncomeExpenseActivity.class);
-                startActivity(addIncExpIntent);
-                break;
-            case R.id.nav_incomes:
-                Intent incomesIntent = new Intent(MainActivity.this, IncomeDisplayActivity.class);
-                startActivity(incomesIntent);
-                break;
-        }
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
-    }
 }
