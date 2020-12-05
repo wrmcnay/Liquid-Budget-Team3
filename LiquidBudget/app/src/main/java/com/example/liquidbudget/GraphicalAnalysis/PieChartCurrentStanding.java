f93bf79a2a02633aab0eb676d20b4e0a2cc09ea2
weston <westonphillips123@gmail.com>
2020-12-04 18:21:00 -0600

    Graphical Analysis UI Updates

---
+++ LiquidBudget/app/src/main/java/com/example/liquidbudget/GraphicalAnalysis/PieChartCurrentStanding.java
@@ -0,0 +1,15 @@
+package com.example.liquidbudget.GraphicalAnalysis;
+
+import android.os.Bundle;
+
+import com.example.liquidbudget.R;
+import com.example.liquidbudget.ui.main.AppBaseActivity;
+
+public class PieChartCurrentStanding extends AppBaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_graphs_view);
+    }
+}
