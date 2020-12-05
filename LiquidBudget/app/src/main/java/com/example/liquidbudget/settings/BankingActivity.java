3ff6c21222491f82509e97667bd3c883c4eed2d6
Riley McNay <wrmcnay@att.net>
2020-12-04 18:11:43 -0600

    cleaning up settings pages

--- LiquidBudget/app/src/main/java/com/example/liquidbudget/settings/BankingActivity.java
+++ LiquidBudget/app/src/main/java/com/example/liquidbudget/settings/BankingActivity.java
@@ -5,12 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.liquidbudget.R;
+import com.example.liquidbudget.ui.main.AppBaseActivity;
 
-public class BankingActivity extends AppCompatActivity {
+public class BankingActivity extends AppBaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTitle("Banking");
         setContentView(R.layout.activity_banking);
     }
 }
