404fcd86fcc62cb80f035ca1c9bbe27c90780d83
Tim Sun <tianyi.sun4@gmail.com>
2021-03-02 19:41:09 -0600

    bottom navigation bar

--- LiquidBudget/app/src/main/java/com/example/liquidbudget/ui/main/AppNavigationActivity.java
+++ LiquidBudget/app/src/main/java/com/example/liquidbudget/ui/main/AppNavigationActivity.java
@@ -10,8 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 
-import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.liquidbudget.CategoryActivity;
 import com.example.liquidbudget.ExpenseDisplayActivity;
@@ -21,31 +20,24 @@ import com.example.liquidbudget.MainActivity;
 import com.example.liquidbudget.R;
 import com.example.liquidbudget.budget.Budget;
 import com.example.liquidbudget.settings.SettingsActivity;
-import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-
-
-public abstract class AppBaseActivity extends AppNavigationActivity implements MenuItem.OnMenuItemClickListener {
-    private FrameLayout view_stub; //This is the framelayout to keep your content view
-    private NavigationView navigation_view; // The new navigation view from Android Design Library. Can inflate menu resources. Easy
-    private DrawerLayout mDrawerLayout;
-    private ActionBarDrawerToggle mDrawerToggle;
-    private Menu drawerMenu;
+public abstract class AppNavigationActivity extends AppCompatActivity implements MenuItem.OnMenuItemClickListener {
+    private FrameLayout view_stub2; //This is the framelayout to keep your content view
+    private BottomNavigationView bottomNavigationView;
+    private Menu bottommenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        super.setContentView(R.layout.base_layout);// The base layout that contains your navigation drawer.
-        view_stub = (FrameLayout) findViewById(R.id.view_stub);
-        navigation_view = (NavigationView) findViewById(R.id.nav_view);
-        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, 0, 0);
-        mDrawerLayout.addDrawerListener(mDrawerToggle);
+        super.setContentView(R.layout.base_layout_bottom);
+        view_stub2 = (FrameLayout) findViewById(R.id.view_stub2);
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        drawerMenu = navigation_view.getMenu();
-        for(int i = 0; i < drawerMenu.size(); i++) {
-            drawerMenu.getItem(i).setOnMenuItemClickListener(this);
+        bottommenu = bottomNavigationView.getMenu();
+        for(int i = 0; i < bottommenu.size(); i++) {
+            bottommenu.getItem(i).setOnMenuItemClickListener(this);
         }
         // and so on...
     }
@@ -53,57 +45,43 @@ public abstract class AppBaseActivity extends AppNavigationActivity implements M
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
-        mDrawerToggle.syncState();
     }
 
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
-        mDrawerToggle.onConfigurationChanged(newConfig);
     }
 
-    /* Override all setContentView methods to put the content view to the FrameLayout view_stub
+    /* Override all setContentView methods to put the content view to the FrameLayout view_stub2
      * so that, we can make other activity implementations looks like normal activity subclasses.
      */
     @Override
     public void setContentView(int layoutResID) {
-        if (view_stub != null) {
+        if (view_stub2 != null) {
             LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
             ViewGroup.LayoutParams lp = new ViewGroup.LayoutParams(
                     ViewGroup.LayoutParams.MATCH_PARENT,
                     ViewGroup.LayoutParams.MATCH_PARENT);
-            View stubView = inflater.inflate(layoutResID, view_stub, false);
-            view_stub.addView(stubView, lp);
+            View stubView = inflater.inflate(layoutResID, view_stub2, false);
+            view_stub2.addView(stubView, lp);
         }
     }
 
     @Override
     public void setContentView(View view) {
-        if (view_stub != null) {
+        if (view_stub2 != null) {
             ViewGroup.LayoutParams lp = new ViewGroup.LayoutParams(
                     ViewGroup.LayoutParams.MATCH_PARENT,
                     ViewGroup.LayoutParams.MATCH_PARENT);
-            view_stub.addView(view, lp);
+            view_stub2.addView(view, lp);
         }
     }
 
     @Override
     public void setContentView(View view, ViewGroup.LayoutParams params) {
-        if (view_stub != null) {
-            view_stub.addView(view, params);
+        if (view_stub2 != null) {
+            view_stub2.addView(view, params);
         }
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Pass the event to ActionBarDrawerToggle, if it returns
-        // true, then it has handled the app icon touch event
-        if (mDrawerToggle.onOptionsItemSelected(item)) {
-            return true;
-        }
-        // Handle your other action bar items...
-
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -153,7 +131,6 @@ public abstract class AppBaseActivity extends AppNavigationActivity implements M
                     finish();
                 break;
         }
-        mDrawerLayout.closeDrawers();
         return false;
     }
 }
